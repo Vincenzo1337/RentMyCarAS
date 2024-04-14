c1ba9e486dd0c9de871a2a0e576821cdce117ac3
Vincenzo1337 <vincentbeurskens89@gmail.com>
2024-04-14 16:57:40 +0200

    description car toegevoegd

--- app/src/main/java/com/example/rentmycaras/models/car.kt
+++ app/src/main/java/com/example/rentmycaras/models/car.kt
@@ -11,6 +11,7 @@ data class Car(
     val category: CarCategory,
     val availability: Boolean,
     val timeBlock: List<TimeBlock>,
+    val description: String,
     val ownerId: Int,
     val owner: User
 //    val photos: List<String>
